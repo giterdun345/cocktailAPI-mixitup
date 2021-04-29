b346d97aaa1779e4ab443e0a06c29654a28f52a7
John Ketterer <johnmketterer@gmail.com>
2021-04-28 22:36:05 -0500

    favicon added with public update

---
+++ mixitup/src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Header from './Header';
+import Landing from './Landing';
+
+
+function App() {
+  return (
+    <div className='container'>
+        <Header />
+        <Landing />
+    </div>
+  );
+}
+
+export default App;
