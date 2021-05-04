7096f8603b81a402d3dea4f0fa3dc0518c437141
John Ketterer <johnmketterer@gmail.com>
2021-05-04 18:25:10 -0500

    ingredient detail complete

--- mixitup/src/cocktails/CocktailCardList.js
+++ mixitup/src/cocktails/CocktailCardList.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-const CocktailCardList= ({info, idxKey})=>{
-
+const CocktailCardList= ({info, idxKey, fromIngredients})=>{
   return(   
     <article key={idxKey} className="list-card">
-        <Link to={`/names-detail/${info.idDrink}`} alt='cocktail image click for details' key={info.idDrink}>
+        <Link to={`/names-detail/${info.idDrink}`} alt='cocktail image click for details' key={info.idDrink} style={fromIngredients ? {height:"330px"} : null}>
           <img src={`${info.strDrinkThumb}/preview`} alt={`cocktail named ${info.strDrink}`} key={`${info.strDrink}${idxKey}`}/>
           <h2 key={info.strDrink}>{info.strDrink}</h2>
           <h3 key={`${idxKey + 200}`}>{info.strAlcoholic}</h3>
--- mixitup/src/ingredients/IngredientDetail.js
+++ mixitup/src/ingredients/IngredientDetail.js
@@ -8,18 +8,18 @@ import CocktailCardList from '../cocktails/CocktailCardList';
 
 
 const IngredientDetail = () => {
-
+//  provides a list of drinks that can be made from the given ingredient 
   const ingredient = useParams().ingredient
   const { data, status } = useDrinkByIngredient(ingredient)
   const drinks = data.drinks
-  console.log(drinks)
+   
   return ( 
     <div>
       <Header dark={true} />
       <ContentHeader text={`Drinks Using ${ingredient}`} />
       <div className='list-container'>
           <section className="list-cards">
-            {drinks ? drinks.map((element, index)=> <CocktailCardList info={element} idxKey={index} fromIngredients={true} />) : <h2 key='345'> {status.toUpperCase()}:But nothing here yet...</h2>}
+            {drinks ? drinks.map((element, index)=> <CocktailCardList info={element} key={index} idxKey={index * 25} fromIngredients={true} />) : <h2 key='345'> {status.toUpperCase()}:But nothing here yet...</h2>}
           </section>
         </div>
     </div>
