38b5673a28ac4704d7c07a3222796aa58fc5e90a
John Ketterer <johnmketterer@gmail.com>
2021-05-04 18:55:14 -0500

    adjust font size detail header

--- mixitup/src/landing/LandingContent.js
+++ mixitup/src/landing/LandingContent.js
@@ -20,14 +20,14 @@ const LandingContent = ()=>{
             <div style={{height:"5vw", marginBottom:"6vh"}}>
             <SearchBar />
             </div>
-            <p style={{fontSize:"1.5vw"}}>Some examples include Long Island Iced Tea, Butterfly Effect, Negroni and New Moon. Throw in a random name and see what happens. </p>
+            <p style={{fontSize:"1.5vw"}}>Some examples include Long Island Iced Tea, Butterfly Effect, Negroni and New Moon. Throw in a part of a name and see what happens. </p>
         </div>
       </div>
       {/* row 2  */}
       <div className='landing-content'>
       <div className='content-text'>
             <p> Browse through some ingredients to learn more about it and what drinks it can make. Maybe you have something laying around and you want to use it?</p>
-            <Button to='#' text='Browse Ingredients' formType='content-btn-form' classType='content-btn' />
+            <Button to='ingredients' text='Browse Ingredients' formType='content-btn-form' classType='content-btn' />
         </div>
         <div className='content-image'>
             <img style={{height:"35vw", border: "1px solid #ddd", borderRadius:"50%"}} src={lemonLime} alt='four cocktails lined up, ready to go' />
