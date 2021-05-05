f64b4e9aab3aa14a90c4578ac464e0933d099a29
John Ketterer <johnmketterer@gmail.com>
2021-05-05 13:22:59 -0500

    build update

--- mixitup/src/reusables/Button.js
+++ mixitup/src/reusables/Button.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 const Button = (props)=>{
   // form type differs from landing hero and landing content by margin (formType) and color (classType)
   return(
-    <Link to={props.to}className={props.formType} >
+    <a href={props.to}className={props.formType} >
       <button type="submit" className={`${props.classType} lb-2`}>{props.text}</button>
-    </Link>
+    </a>
   )
 }
 
