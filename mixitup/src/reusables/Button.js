daec2dc71d833232b3fef9f76f24173dd1f2a259
John Ketterer <johnmketterer@gmail.com>
2021-05-05 13:15:22 -0500

    button link fix

--- mixitup/src/reusables/Button.js
+++ mixitup/src/reusables/Button.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Button = (props)=>{
   // form type differs from landing hero and landing content by margin (formType) and color (classType)
   return(
-    <form action={props.to}className={props.formType} >
+    <Link action={props.to}className={proLinkType} >
       <button type="submit" className={`${props.classType} lb-2`}>{props.text}</button>
-    </form>
+    </Link>
   )
 }
 
